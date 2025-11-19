fe82fb42f6bcc735954dc936f6a65f6591e8aef9
ketchup0211 <dlcka1111@naver.com>
2025-11-19 22:59:30 +0900

    [ADD] Header components psudo

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Profile from "../pages/profile/Profile";
 import BottomNav from "../components/layout/BottomNav";
+import Header from "../components/layout/Header";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
