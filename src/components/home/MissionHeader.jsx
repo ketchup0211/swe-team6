ea33d366f65bd6103850b49f813f9be1626d470c
ketchup0211 <dlcka1111@naver.com>
2025-11-20 00:03:41 +0900

    [MOD] MissionHeader CSS

--- src/components/home/MissionHeader.jsx
+++ src/components/home/MissionHeader.jsx
@@ -2,17 +2,17 @@ import styles from "./MissionHeader.module.css";
 
 function MissionHeader({ succedDays, leftDays, goal, prize }) {
   return (
-    <div>
-      <div>
-        <p>
+    <div className={styles.container}>
+      <div className={styles.missionContainer}>
+        <p className={styles.daysContainer}>
           <span className={styles.days}>{succedDays}</span> 일째 도전 중이에요!{" "}
           <span className={styles.days}>{leftDays}</span>일 남았어요
         </p>
-        <p>{goal}</p>
+        <p className={styles.goal}>{goal}</p>
       </div>
-      <div>
+      <div className={styles.prizeContainer}>
         <p>보상 사진</p>
-        <p>{prize}</p>
+        <p className={styles.prize}>{prize}</p>
       </div>
     </div>
   );
