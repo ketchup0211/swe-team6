20fa04cb947ff6f22b702990666c1a27bfff7c78
ketchup0211 <dlcka1111@naver.com>
2025-11-25 01:40:59 +0900

    [ADD] svg notion-icon as react component

--- src/components/home/Notion.jsx
+++ src/components/home/Notion.jsx
@@ -1,11 +1,11 @@
 import styles from "./Notion.module.css";
-import NotionIcon from "../../assets/notion-icon.svg";
+import NotionIcon from "../../assets/notion-icon.svg?react";
 
 function Notion({ notion }) {
   return (
     <div id={styles.container}>
       <div id={styles.notionContainer}>
-        <p>공지 아이콘</p>
+        <NotionIcon />
         <p className={styles.text}>{notion}</p>
       </div>
       <p>토글 아이콘</p>
