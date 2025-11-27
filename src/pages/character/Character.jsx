7ad8d4624b4f45173bf9a34da196b17d1621cf5c
ketchup0211 <dlcka1111@naver.com>
2025-11-27 23:04:34 +0900

    [ADD] star-icon.png

--- src/pages/character/Character.jsx
+++ src/pages/character/Character.jsx
@@ -1,7 +1,27 @@
 import { useState, useEffect } from "react";
 
 function Character() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>character background</h1>
+        <div>
+          <div>
+            <p>나의 도전</p>
+            <p>공지</p>
+          </div>
+          <p>point</p>
+        </div>
+        <div>
+          <p>character message</p>
+          <p>character</p>
+        </div>
+      </div>
+      <div>
+        <p>bottom bar</p>
+      </div>
+    </div>
+  );
 }
 
 export default Character;
