10e4082211eba263574ac746f18262b2a31eced4
ketchup0211 <dlcka1111@naver.com>
2025-12-02 03:27:50 +0900

    [MOD] button css

--- src/components/character/ShortcutButton.jsx
+++ src/components/character/ShortcutButton.jsx
@@ -1,12 +1,10 @@
-import styles from "./ShortcutButton.module.css";
 import starIcon from "../../assets/star-icon.png";
 import Button from "../../components/ui/Button";
 
 function ShortcutButton({ name, url = "/" }) {
   return (
     <span>
-      <img className={styles.gotoIcon} src={starIcon} alt="star-icon" />
-      <Button className={styles.buttonText} name={name} url={url} />
+      <Button name={name} url={url} icon={starIcon} />
     </span>
   );
 }
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
-export default function Button({ name, url }) {
+import styles from "./Button.module.css";
+export default function Button({ name, url, icon = null }) {
   return (
     <div>
       <Link to={url}>
-        <button type="button">{name}</button>
+        <button className={styles.button} type="button">
+          {icon === null ? null : (
+            <img className={styles.gotoIcon} src={icon} alt="button-icon" />
+          )}
+          {name}
+        </button>
       </Link>
     </div>
   );
