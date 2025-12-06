1e8714710078b73c96767d148fd48ce19cfacbb6
Ellsy23 <dlwnsgus3153@naver.com>
2025-12-06 20:46:40 +0900

    fix: PropTypes 추가, 사용하지 않는 props 제거

--- src/components/character/Avatar.jsx
+++ src/components/character/Avatar.jsx
@@ -1,9 +1,10 @@
-import styles from "./Avatar.module.css";
-import avatar from "../../assets/avatar/avatar.png";
-import MessagePoint from "../../assets/message-point.svg?react";
-import AvatarShade from "../../assets/avatar/avatar-shade.svg?react";
+import PropTypes from 'prop-types';
+import styles from './Avatar.module.css';
+import avatar from '../../assets/avatar/avatar.png';
+import MessagePoint from '../../assets/message-point.svg?react';
+import AvatarShade from '../../assets/avatar/avatar-shade.svg?react';
 
-function Avatar({ alert }) {
+function Avatar() {
   const friend = "지현이";
   const hasMessage = friend !== "";
 
@@ -31,5 +32,13 @@ function Avatar({ alert }) {
     </div>
   );
 }
+
+Avatar.propTypes = {
+  alert: PropTypes.arrayOf(PropTypes.string),
+};
+
+Avatar.defaultProps = {
+  alert: [],
+};
 
 export default Avatar;
