4fd83ced3134b77f0fa1c38526162fb4718b859b
Ellsy23 <dlwnsgus3153@naver.com>
2025-12-06 20:48:53 +0900

    refactor: ROUTES 상수 사용, 일관된 코드 스타일

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/home/Home";
-import Profile from "../pages/profile/Profile";
-import Character from "../pages/character/Character";
-import MyChallenge from "../pages/my-challenge/MyChallenge";
-import CreateChallenge from "../pages/create-challenge/CreateChallenge";
-import Layout from "../components/layout/Layout";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/home/Home';
+import Profile from '../pages/profile/Profile';
+import Character from '../pages/character/Character';
+import MyChallenge from '../pages/my-challenge/MyChallenge';
+import CreateChallenge from '../pages/create-challenge/CreateChallenge';
+import Layout from '../components/layout/Layout';
+import { ROUTES } from '../constants/routes';
 
-const Router = () => {
+function Router() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/character" element={<Character />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/my-challenge" element={<MyChallenge />} />
-          <Route path="/create-challenge" element={<CreateChallenge />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.CHARACTER} element={<Character />} />
+          <Route path={ROUTES.PROFILE} element={<Profile />} />
+          <Route path={ROUTES.MY_CHALLENGE} element={<MyChallenge />} />
+          <Route path={ROUTES.CREATE_CHALLENGE} element={<CreateChallenge />} />
         </Routes>
       </Layout>
     </BrowserRouter>
   );
-};
+}
 
 export default Router;
